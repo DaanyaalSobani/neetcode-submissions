fa55da80948aa6275a9c57f080d39c1c5ad699cb
Daanyaal Sobani <daanyaal.sobani@gmail.com>
2026-04-20 20:27:04 -0400

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,7 @@
+class Solution:
+# memory O(1) attempt
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        return sorted(s) == sorted(t)
+        
